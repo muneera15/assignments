6fc7c827586d67f4e332e1d672a9c0a06e8d48e4
muneera <muneerashaik03@gmail.com>
2024-01-09 23:24:39 +0530

    completed medium assignments of week 1

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -7,6 +7,14 @@
 
 function countVowels(str) {
     // Your code here
+    const cleanStr = str.replace(/\s/g,'').toLowerCase();
+    let vowels= 0;
+    for(i=0; i<cleanStr.length;i++){
+      if(cleanStr[i]=== 'a' || cleanStr[i]==='e' || cleanStr[i]==='i' || cleanStr[i]==='o' || cleanStr[i]==='u'){
+        vowels = vowels +1;
+      }
+    }
+    return vowels;
 }
 
 module.exports = countVowels;
