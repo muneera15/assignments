ec850459bee0d6ca4935164581e2e8d30bde61e3
muneera <muneerashaik03@gmail.com>
2024-01-12 00:25:41 +0530

    todoServer.js done

--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -45,5 +45,53 @@
   const app = express();
   
   app.use(bodyParser.json());
+
+  let todos =[];
+
+  app.get('./todos',(req,res)=>{
+    res.json(todos)
+  });
+
+  app.get('./todos/:id',(req,res)=>{
+    const todo = todo.find(t => t.id === parseInt(req.params.id));
+    if(!todo){
+      res.status(404).send("not found")
+    }
+    res.send(todo)
+    })
+
+    app.post('./todos',(req,res)=>{
+      const newTodo = {
+        id : Math.floor(Math.random * 1000000),
+        title : req.body.title,
+        description : req.body.description
+      };
+      todos.push(newTodo);
+      res.status(201).json(newTodo);
+    });
+
+    app.put('./todos',(req,res)=> {
+      const todoIndex = todos.findIndex(t => t.id = parseInt(req.params.id));
+      if( todoIndex === -1){
+        res.status(404).send("not found");
+      }
+      todos[todoIndex].title = req.body.title,
+      todos[todoIndex].description = req.body.description
+      res.json(todos[todoIndex]);
+    });
+
+    app.delete('/todos/:id',(req, res)=>{
+      const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
+      if (todoIndex === -1) {
+        res.status(404).send();
+      }
+        todos.splice(todoIndex, 1);
+        res.status(200).send();
+    });
+    
+    app.use(req,res,next => {
+      res.status(404).send('route not found');
+    });
+
   
   module.exports = app;
