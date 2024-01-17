717327385d123905067d0269a872a0b7a871173b
muneera <muneerashaik03@gmail.com>
2024-01-17 17:11:07 +0530

    03-mongo

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,34 @@
 const mongoose = require('mongoose');
+const { string } = require('zod');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://muneerashaik03:muneera03@cluster0.ckq1avn.mongodb.net/course_selling_app');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password : String
 });
 
 const UserSchema = new mongoose.Schema({
+    username : String,
+    password : String,
+    purchasedCourses : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "course"
+    }]
     // Schema definition here
 });
 
 const CourseSchema = new mongoose.Schema({
+
     // Schema definition here
+    title : String,
+    Description : String,
+    imageLink : String,
+    price : Number
+
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
